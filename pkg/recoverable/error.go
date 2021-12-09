4645866f885dd3cfb75982069d5e50e40bf97ae1
Jaesang Kim <honeysleep@gmail.com>
2021-12-09 22:45:11 +0900

    Deprecate github.com/pkg/errors (#1)

--- pkg/recoverable/error.go
+++ pkg/recoverable/error.go
@@ -2,9 +2,8 @@ package recoverable
 
 import (
 	"fmt"
-	"github.com/pkg/errors"
 )
 
 func toError(r interface{}) error {
-	return errors.New(fmt.Sprintf("%v", r))
+	return fmt.Errorf("%v", r)
 }
--- pkg/recoverable/waitgroup_test.go
+++ pkg/recoverable/waitgroup_test.go
@@ -2,8 +2,9 @@ package recoverable
 
 import (
 	"fmt"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestWaitGroup_Wait(t *testing.T) {
